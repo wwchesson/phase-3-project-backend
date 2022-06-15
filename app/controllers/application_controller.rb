700738eb2fdd42c059caa2c553f622c32b4abf84
Will <willchesson@gmail.com>
2022-06-15 12:09:27 -0400

    final touches

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
+require "pry"
+
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add your routes here
   get "/residents" do
     residents = Resident.all 
     residents.to_json
@@ -9,7 +10,7 @@ class ApplicationController < Sinatra::Base
 
   get "/residents/:id" do
     resident = Resident.find(params[:id])
-    resident.to_json
+    resident.to_json(include: :activities)
   end
 
   post "/residents" do
@@ -31,11 +32,11 @@ class ApplicationController < Sinatra::Base
 
   get "/activities" do
     activity = Activity.all
-    activity.to_json(include: :resident)
+    activity.to_json
   end
 
-  post "/activities" do
-    activity = Activity.create(activity: params[:activity], day_of_week: params[:day_of_week], instructor: params[:instructor], resident_name: params[:resident_name])
+  post "/activities" do 
+    activity = Activity.create(activity: params[:activity], day_of_week: params[:day_of_week], instructor: params[:instructor], resident_id: params[:resident_id])
     activity.to_json
   end
   
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,20 +6,20 @@ puts "🌱 Seeding spices..."
         ssn: Faker::Number.number(digits: 9),
         age: Faker::Number.within(range: 18..70),
         email: Faker::Internet.safe_email,
-        needs: Faker::Lorem.paragraphs(number: 1),
-        interests: Faker::Lorem.paragraphs(number: 1)
+        needs: Faker::Lorem.paragraph(sentence_count: 2),
+        interests: Faker::Lorem.paragraph(sentence_count: 3)
     )
 )
 end
 
-Activity.create(activity: "Gardening", day_of_week: "Monday", instructor: Faker::Name.name, resident_id: Faker::Number.within(range: 51..60))
-Activity.create(activity: "Classes", day_of_week: "Tuesday", instructor: Faker::Name.name, resident_id: Faker::Number.within(range: 51..60))
-Activity.create(activity: "Cooking", day_of_week: "Wednesday", instructor: Faker::Name.name, resident_id: Faker::Number.within(range: 51..60))
-Activity.create(activity: "Exercise", day_of_week: "Thursday", instructor: Faker::Name.name, resident_id: Faker::Number.within(range: 51..60))
-Activity.create(activity: "Counseling", day_of_week: "Friday", instructor: Faker::Name.name, resident_id: Faker::Number.within(range: 51..60))
-Activity.create(activity: "Construction", day_of_week: "Monday", instructor: Faker::Name.name, resident_id: Faker::Number.within(range: 51..60))
-Activity.create(activity: "Study Hall", day_of_week: "Tuesday", instructor: Faker::Name.name, resident_id: Faker::Number.within(range: 51..60))
-Activity.create(activity: "Cleaning", day_of_week: "Wednesday", instructor: Faker::Name.name, resident_id: Faker::Number.within(range: 51..60))
+Activity.create(activity: "Gardening", day_of_week: "Monday", instructor: Faker::Name.name, resident_id: Faker::Number.within(range: 1..10))
+Activity.create(activity: "Classes", day_of_week: "Tuesday", instructor: Faker::Name.name, resident_id: Faker::Number.within(range: 1..10))
+Activity.create(activity: "Cooking", day_of_week: "Wednesday", instructor: Faker::Name.name, resident_id: Faker::Number.within(range: 1..10))
+Activity.create(activity: "Exercise", day_of_week: "Thursday", instructor: Faker::Name.name, resident_id: Faker::Number.within(range: 1..10))
+Activity.create(activity: "Counseling", day_of_week: "Friday", instructor: Faker::Name.name, resident_id: Faker::Number.within(range: 1..10))
+Activity.create(activity: "Construction", day_of_week: "Monday", instructor: Faker::Name.name, resident_id: Faker::Number.within(range: 1..10))
+Activity.create(activity: "Study Hall", day_of_week: "Tuesday", instructor: Faker::Name.name, resident_id: Faker::Number.within(range: 1..10))
+Activity.create(activity: "Cleaning", day_of_week: "Wednesday", instructor: Faker::Name.name, resident_id: Faker::Number.within(range: 1..10))
 
 
 
